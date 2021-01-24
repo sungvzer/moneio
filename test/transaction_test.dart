49dbf62d6c0034b160c949cb4429d792d211a2da
sungvzer <garganosalvio@gmail.com>
2021-01-24 14:48:42 +0100

    Tests: transaction_test for getSeparatedAmountString() now takes negative values into account. Blunder by my side.

--- test/transaction_test.dart
+++ test/transaction_test.dart
@@ -2,44 +2,80 @@ import 'package:test/test.dart';
 import 'package:moneio/models/transaction.dart';
 
 main() {
-  Transaction a = new Transaction(amount: 0.0);
-  Transaction b = new Transaction(amount: 1.0);
+  Transaction a = new Transaction(amount: 1.0);
+  Transaction aNegative = new Transaction(amount: -1.0);
+
   Transaction c = new Transaction(amount: 10.0);
+  Transaction cNegative = new Transaction(amount: -10.0);
+
   Transaction d = new Transaction(amount: 100.0);
+  Transaction dNegative = new Transaction(amount: -100.0);
+
   Transaction e = new Transaction(amount: 1000.0);
+  Transaction eNegative = new Transaction(amount: -1000.0);
+
   Transaction f = new Transaction(amount: 10000.0);
+  Transaction fNegative = new Transaction(amount: -10000.0);
+
   Transaction g = new Transaction(amount: 100000.0);
+  Transaction gNegative = new Transaction(amount: -100000.0);
+
   Transaction h = new Transaction(amount: 1000000.0);
+  Transaction hNegative = new Transaction(amount: -1000000.0);
 
-  group('getSeparatedAmountString', () {
-    test('Single digit - 0', () {
-      expect(a.getSeparatedAmountString(), '0,00');
+  group('positive getSeparatedAmountString', () {
+    test('Positive: Single digit - 1', () {
+      expect(a.getSeparatedAmountString(), '1,00');
     });
 
-    test('Single digit - 1', () {
-      expect(b.getSeparatedAmountString(), '1,00');
-    });
-
-    test('Two digits - 10', () {
+    test('Positive: Two digits - 10', () {
       expect(c.getSeparatedAmountString(), '10,00');
     });
 
-    test('Three digits - 100', () {
+    test('Positive: Three digits - 100', () {
       expect(d.getSeparatedAmountString(), '100,00');
     });
 
-    test('Four digits - 1000', () {
+    test('Positive: Four digits - 1000', () {
       expect(e.getSeparatedAmountString(), '1.000,00');
     });
 
-    test('Five digits - 10000', () {
+    test('Positive: Five digits - 10000', () {
       expect(f.getSeparatedAmountString(), '10.000,00');
     });
-    test('Six digits - 100000', () {
+    test('Positive: Six digits - 100000', () {
       expect(g.getSeparatedAmountString(), '100.000,00');
     });
-    test('Seven digits - 1000000', () {
+    test('Positive: Seven digits - 1000000', () {
       expect(h.getSeparatedAmountString(), '1.000.000,00');
+    });
+  });
+  group('negative getSeparatedAmountString', () {
+    test('Negative: Single digit - 1', () {
+      expect(aNegative.getSeparatedAmountString(), '1,00');
+    });
+
+    test('Negative: Two digits - 10', () {
+      expect(cNegative.getSeparatedAmountString(), '10,00');
+    });
+
+    test('Negative: Three digits - 100', () {
+      expect(dNegative.getSeparatedAmountString(), '100,00');
+    });
+
+    test('Negative: Four digits - 1000', () {
+      expect(eNegative.getSeparatedAmountString(), '1.000,00');
+    });
+
+    test('Negative: Five digits - 10000', () {
+      expect(fNegative.getSeparatedAmountString(), '10.000,00');
+    });
+    test('Negative: Six digits - 100000', () {
+      var r = gNegative.getSeparatedAmountString();
+      expect(gNegative.getSeparatedAmountString(), '100.000,00');
+    });
+    test('Negative: Seven digits - 1000000', () {
+      expect(hNegative.getSeparatedAmountString(), '1.000.000,00');
     });
   });
 }

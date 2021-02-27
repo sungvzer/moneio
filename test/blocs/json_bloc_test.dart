72d274f01022e96a7d0f93ded3a028f6fe7e6231
sungvzer <garganosalvio@gmail.com>
2021-02-27 12:31:52 +0100

    Bloc tests: reflect changes in application.
    - Use getApplicationDocumentsDirectory() to clear file.
    - Remove unused data/test.json file

--- test/blocs/json_bloc_test.dart
+++ test/blocs/json_bloc_test.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:moneio/bloc/json/json_bloc.dart';
 
 main() async {
@@ -8,8 +9,10 @@ main() async {
 
   /*We create and delete the file to make sure it
    is not present at the time of running 'JsonWrite'*/
-  await File(fileName).create(recursive: true);
-  await File(fileName).delete(recursive: true);
+  await File((await getApplicationDocumentsDirectory()).path + '/' + fileName)
+      .create(recursive: true);
+  await File((await getApplicationDocumentsDirectory()).path + '/' + fileName)
+      .delete(recursive: true);
 
   Map<String, dynamic> validJson = {
     "id": 0,
@@ -20,7 +23,6 @@ main() async {
     JsonBloc x = JsonBloc();
     x.listen((state) {
       expect(state.isError, false);
-      expect(state.message, "Written value: ${state.value}");
     });
     x.add(JsonWrite(
       fileName,
@@ -35,7 +37,7 @@ main() async {
     x.listen((state) {
       expect(state.isError, false);
       expect(state.hasValue, true);
-      expect(state.value, validJson);
+      expect(state.value, [validJson]);
     });
 
     x.add(JsonRead(fileName));

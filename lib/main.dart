d3cc302235a9f69dc8c7ef4c54f8bdb4e4cda193
sungvzer <garganosalvio@gmail.com>
2021-03-12 22:37:44 +0100

    Main: add various random transactions to file if debugging

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:moneio/bloc/json/json_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moneio/constants.dart';
+import 'package:moneio/models/transaction.dart';
 import 'package:moneio/views/home_page.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -39,19 +42,25 @@ class Application extends StatelessWidget {
     loadPaths();
     // ignore: close_sinks
     JsonBloc b = BlocProvider.of<JsonBloc>(context);
-    if (kDebugMode)
+    if (kDebugMode) b.add(JsonClear("transactions.json"));
+    for (var i = 0; i < 30; i++) {
+      var keys = categories.toList();
+      keys.shuffle();
+
+      Transaction newTransaction = Transaction(
+        category: keys[0],
+        currency: currencyToSymbol.keys
+            .elementAt(Random().nextInt(currencyToSymbol.length)),
+        amount: Random().nextInt(20e7.toInt()) - 10e7.toInt(),
+        date: DateTime(Random().nextInt(50) + 1970, Random().nextInt(12),
+            Random().nextInt(27)),
+        tag: String.fromCharCodes(
+            List.generate(30, (index) => Random().nextInt(33) + 89)),
+      );
+
       b.add(JsonWrite("transactions.json",
-          value: {
-            "amount": 320,
-            "date": DateTime(2020, 05, 02, 10, 10).toIso8601String(),
-            "icon": "🍕",
-            "tag": "Pizzamandolino",
-            "category": categories
-                .firstWhere((category) => category.uniqueID == "FOOD"),
-            "currency": "EUR"
-          },
-          append: false,
-          createFileIfNeeded: true));
+          append: true, value: newTransaction.toMap()));
+    }
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,

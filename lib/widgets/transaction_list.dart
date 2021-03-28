f87e27a6333e77a464f4848f5e19ca60e68969fe
sungvzer <garganosalvio@gmail.com>
2021-03-28 12:01:14 +0200

    Widgets: compute splitting index in TransactionListTile

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -5,6 +5,7 @@ import 'package:moneio/bloc/preference/preference_bloc.dart';
 import 'package:moneio/color_palette.dart';
 import 'package:moneio/constants.dart';
 import 'package:moneio/models/transaction.dart';
+import 'package:moneio/screen.dart';
 
 class TransactionListBuilder extends StatefulWidget {
   const TransactionListBuilder();
@@ -144,21 +145,28 @@ class _TransactionTile extends StatelessWidget {
     final DateTime date = _current.date;
     final int amount = _current.amount;
 
-    // TODO: compute MAX_AMOUNT_LENGTH based on device width?!
-    const int MAX_AMOUNT_LENGTH = 10;
+    debugPrint(
+        "_TransactionTile.build(): device width is ${screenWidth(context).round()}");
+
+    int maxAmountLength = screenWidth(context).round();
+    maxAmountLength = (maxAmountLength / 56).floor();
+    debugPrint(
+        "_TransactionTile.build(): so the new length is $maxAmountLength");
+
     String amountString = _current.getSeparatedAmountString(
         sign: true, currency: true, humanReadable: _humanReadable);
 
-    // This is a little hack to make TextOverflow work
-    // on a single word string.
-    // Example:
-    // '+$30,000,000.00' -> '+$30,000[space],000.00'
-    if (amountString.length > MAX_AMOUNT_LENGTH) {
-      amountString = amountString.substring(0, MAX_AMOUNT_LENGTH) +
-          ' ' +
-          amountString.substring(MAX_AMOUNT_LENGTH);
+    if (!_humanReadable) {
+      // This is a little hack to make TextOverflow work
+      // on a single word string.
+      // Example:
+      // '+$30,000,000.00' -> '+$30,000[space],000.00'
+      if (amountString.length > maxAmountLength) {
+        amountString = amountString.substring(0, maxAmountLength) +
+            ' ' +
+            amountString.substring(maxAmountLength);
+      }
     }
-
     return ListTile(
       // TODO: Transaction deletion and edit
       onLongPress: () => print("TODO: Long press with transaction $_current"),

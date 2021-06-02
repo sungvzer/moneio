8221732b9db26347bd0b37da8458a8ee669e2cc8
sungvzer <garganosalvio@gmail.com>
2021-06-02 23:29:44 +0200

    Helpers: add file for helper functions regarding Flutter `Color`s

---
+++ lib/helpers/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+/// https://stackoverflow.com/a/60191441/7163750
+///
+/// Darken a color by [percent] amount (100 = black)
+Color darken(Color c, [int percent = 10]) {
+  assert(1 <= percent && percent <= 100);
+  var f = 1 - percent / 100;
+  return Color.fromARGB(c.alpha, (c.red * f).round(), (c.green * f).round(),
+      (c.blue * f).round());
+}
+
+/// https://stackoverflow.com/a/60191441/7163750
+///
+/// Lighten a color by [percent] amount (100 = white)
+Color lighten(Color c, [int percent = 10]) {
+  assert(1 <= percent && percent <= 100);
+  var p = percent / 100;
+  return Color.fromARGB(
+      c.alpha,
+      c.red + ((255 - c.red) * p).round(),
+      c.green + ((255 - c.green) * p).round(),
+      c.blue + ((255 - c.blue) * p).round());
+}

06319e7b6c40bd7fdb51e6eefe5be1fc869b00b8
sungvzer <garganosalvio@gmail.com>
2021-06-19 23:53:46 +0200

    Sorting helpers: make use of localized strings

--- lib/helpers/sorting.dart
+++ lib/helpers/sorting.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:moneio/generated/l10n.dart';
 
 enum SortType { ByDate, ByTag, ByAmount, ByCategory }
 enum SortStrategy { Ascending, Descending }
@@ -6,12 +7,12 @@ enum SortStrategy { Ascending, Descending }
 String getSortTypeName(BuildContext context, SortType type) {
   switch (type) {
     case SortType.ByCategory:
-      return "By category";
+      return Localization.of(context).homeSortByCategory;
     case SortType.ByAmount:
-      return "By amount";
+      return Localization.of(context).homeSortByAmount;
     case SortType.ByDate:
-      return "By date";
+      return Localization.of(context).homeSortByDate;
     case SortType.ByTag:
-      return "By tag";
+      return Localization.of(context).homeSortByTag;
   }
 }
